5e223e9e4cbf9d78676a56d1aecfad4f1d015131
anoel <axou.noel@gmail.com>
2023-02-24 16:28:07 +0100

    ajout objectifs

---
+++ src/main/java/fr/ensicaen/lv223/model/agent/robot/objectif/CollectOreObjectif.java
@@ -0,0 +1,33 @@
+package fr.ensicaen.lv223.model.agent.robot.objectif;
+
+import fr.ensicaen.lv223.model.agent.robot.Robot;
+import fr.ensicaen.lv223.model.agent.robot.command.Command;
+import fr.ensicaen.lv223.model.environment.cells.specials.MineralCell;
+
+import java.util.PriorityQueue;
+
+public class CollectOreObjectif implements Objectif{
+
+    private Robot robot;
+
+    private MineralCell focusedCell = null;
+
+    public CollectOreObjectif(Robot currentRobot) {
+        this.robot = currentRobot;
+    }
+
+    @Override
+    public PriorityQueue<Command> generateCommmandList() {
+        PriorityQueue<Command> commandes = new PriorityQueue<>();
+        if(this.focusedCell != null){
+            // Si capacité max atteinte, se rendre au centralisateur
+            // Sinon si sur une cellule de minerais, la miner
+            // Sinon, se rendre sur la cellule de minerais
+        }
+        else{
+            // Si centralisateur connait une case de minerais non utilisée, la récuppérer puis return this.generateCommmandList()
+            // Sinon, se balader vers les cases non découvertes
+        }
+        return commandes;
+    }
+}
