491dcd4d96eeded41bbf90826251ba72a13b3ed5
leboc <lebocq.titouan@gmail.com>
2023-02-25 00:54:32 +0100

    fix on null return in compute.

--- src/main/java/fr/ensicaen/lv223/teams/jamesbond/robot/robots/PipelineBuilderJB.java
+++ src/main/java/fr/ensicaen/lv223/teams/jamesbond/robot/robots/PipelineBuilderJB.java
@@ -1,11 +1,8 @@
 package fr.ensicaen.lv223.teams.jamesbond.robot.robots;
 
-import fr.ensicaen.lv223.model.agent.command.Command;
 import fr.ensicaen.lv223.model.agent.robot.RobotType;
 import fr.ensicaen.lv223.model.agent.robot.message.Message;
 import fr.ensicaen.lv223.model.agent.robot.specials.PipelineBuilder;
-
-import java.util.List;
 
 public class PipelineBuilderJB extends PipelineBuilder {
     public PipelineBuilderJB(RobotType type) {
@@ -15,10 +12,5 @@ public class PipelineBuilderJB extends PipelineBuilder {
     @Override
     public boolean isAvailable( Message m ) {
         return false;
-    }
-
-    @Override
-    public List<Command> compute() {
-        return null;
     }
 }
