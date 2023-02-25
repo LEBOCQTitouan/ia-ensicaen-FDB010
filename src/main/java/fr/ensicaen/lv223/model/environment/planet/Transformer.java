b2cde69f315da0f6eaef0c25aacc319a41837b42
Booillon Nicolas <nicolas.ecole.ensicaen.fr>
2023-02-25 16:44:28 +0100

    Add Transformer for Cells

--- src/main/java/fr/ensicaen/lv223/model/environment/planet/Transformer.java
+++ src/main/java/fr/ensicaen/lv223/model/environment/planet/Transformer.java
@@ -1,7 +1,24 @@
 package fr.ensicaen.lv223.model.environment.planet;
 
+import fr.ensicaen.lv223.model.environment.cells.Cell;
+
+import java.util.List;
+
 /**
  * Transform the case into other case
  */
 public class Transformer {
+    private final List<List<Cell>> cells;
+    public Transformer(List<List<Cell>> cells){
+        this.cells = cells;
+    }
+
+    public void transforme(Cell currenCell) {
+        Cell temp = currenCell.transformation();
+        for(List<Cell> list: this.cells){
+            if(list.contains(currenCell)){
+                list.set(list.indexOf(currenCell), temp);
+            }
+        }
+    }
 }
