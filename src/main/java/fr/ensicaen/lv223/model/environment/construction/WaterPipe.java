921b474ce995115bc0366aa68bf43df362effc0c
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2023-03-01 13:09:56 +0100

    Comment on water pipe on what to do for possible evolutions

--- src/main/java/fr/ensicaen/lv223/model/environment/construction/WaterPipe.java
+++ src/main/java/fr/ensicaen/lv223/model/environment/construction/WaterPipe.java
@@ -3,6 +3,7 @@ package fr.ensicaen.lv223.model.environment.construction;
 import java.util.ArrayList;
 import java.util.List;
 
+// TODO replace the system of singletons for the presenter
 public class WaterPipe {
 
     public static final List<WaterPipe> waterPipes = new ArrayList<>();
