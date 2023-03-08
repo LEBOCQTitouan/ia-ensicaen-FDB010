344f484d3c2f0f5ef453a28512d13afc3f489433
LEBOCQ Titouan <lebocq.titouan@gmail.com>
2023-03-08 19:19:35 +0100

    Removing Neighbor finder from util

--- src/main/java/fr/ensicaen/lv223/util/Util.java
+++ src/main/java/fr/ensicaen/lv223/util/Util.java
@@ -8,35 +8,4 @@ import java.util.logging.Logger;
 
 public class Util {
     public final static Logger LOGGER = Logger.getLogger(Logger.GLOBAL_LOGGER_NAME);
-
-    public static List<Coordinate> getNeighbors(Coordinate coord, int width, int height) {
-        ArrayList<Coordinate> neighbors = new ArrayList<>();
-        int x = coord.x;
-        int y = coord.y;
-        if (x > 0) {
-            neighbors.add(new Coordinate(x - 1, y));
-            if (y > 0) {
-                neighbors.add(new Coordinate(x - 1, y - 1));
-            }
-            if (y < width - 1) {
-                neighbors.add(new Coordinate(x - 1, y + 1));
-            }
-        }
-        if (x < height - 1) {
-            neighbors.add(new Coordinate(x + 1, y));
-            if (y > 0) {
-                neighbors.add(new Coordinate(x + 1, y - 1));
-            }
-            if (y < width - 1) {
-                neighbors.add(new Coordinate(x + 1, y + 1));
-            }
-        }
-        if (y > 0) {
-            neighbors.add(new Coordinate(x, y - 1));
-        }
-        if (y < width - 1) {
-            neighbors.add(new Coordinate(x, y + 1));
-        }
-        return neighbors;
-    }
 }
